5f61cc5ce77606cbf9a8f469464606c74b1ba5c7
Kuroshika <huanglei701@126.com>
2025-02-17 02:45:52 +0000

    添加对pico2024的支持

--- builder/__init__.py
+++ builder/__init__.py
@@ -1,4 +1,1 @@
-from .builder import(
-    ModelRegistry, DatasetRegistry, HeadRegistry,
-    build_model, build_dataset, build_loss, build_optimizer, build_head,
-)
+from .builder import ModelRegistry, DatasetRegistry, HeadRegistry, build_model, build_dataset, build_loss, build_optimizer, build_head
--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,5 +1,6 @@
 from .ut_har_dataset import UT_HAR
 from .ntu_fi import NTU_Fi
+from .pico2024 import Pico2024
 
 # from dataset import ntu_fi
 # from dataset import ut_har_dataset
